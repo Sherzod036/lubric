a2150d8ac646ac97b8d79164d4fb7455284d7cbf
Sherzod <sherzod.erkinov036@gmail.com>
2021-01-06 01:43:22 +0500

    adaptive

--- src/js/script.js
+++ src/js/script.js
@@ -2,26 +2,46 @@ $(() => {
   function fullpageInit() {
     $('#fullpage').fullpage({
       anchors: ['hero', 'categories', 'catalog', 'about', 'mfiles', 'contacts'],
-      sectionsColor: ['#0445a3', '#ffcc29', '#fff', '#0445a3', '#fff', '#0445a3'],
+      sectionsColor: [
+        '#0445a3',
+        '#ffcc29',
+        '#fff',
+        '#0445a3',
+        '#fff',
+        '#0445a3',
+      ],
       scrollingSpeed: 1000,
       navigation: true,
       onLeave: (origin, destination, direction) => {
         let section = destination.item;
         let title = section.querySelector('.title_anime');
 
-        title.innerHTML = title.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+        title.innerHTML = title.textContent.replace(
+          /\S/g,
+          "<span class='letter'>$&</span>"
+        );
 
         // Vars
         let letters = section.querySelectorAll('.letter');
 
         const tl = new TimelineMax({ delay: 0.3 });
 
-        tl.staggerFrom(letters, 0.6, { opacity: 0, ease: Power4.easeInOut }, 0.06, 0);
+        tl.staggerFrom(
+          letters,
+          0.6,
+          { opacity: 0, ease: Power4.easeInOut },
+          0.06,
+          0
+        );
       },
     });
   }
 
-  fullpageInit();
+  const w_width = $(window).width();
+
+  if (w_width > 1200) {
+    fullpageInit();
+  }
 
   // Section active
 
@@ -31,6 +51,13 @@ $(() => {
     e.preventDefault();
     fullpage_api.moveSectionDown();
   });
+
+  let $tablogoZoon = $('.logo_zoon span.tablogo');
+  let $tablogoSynheon = $('.logo_synheon span.tablogo');
+  let $tablogoGraumann = $('.logo_graumann span.tablogo');
+
+  let bW = '1px solid #fff';
+  let bS = '1px solid #d0d0d0';
 
   $('.tabscaption li').on('click', function () {
     $(this)
@@ -42,29 +69,50 @@ $(() => {
       .removeClass('active')
       .eq($(this).index())
       .addClass('active');
+
+    if ($(window).width() > 992) {
+      $('.logo_zoon').css({ borderRight: bS, borderBottom: bS });
+      let _this = $(this);
+
+      if (_this.hasClass('logo_zoon')) {
+        _this.find('.tablogo').css({ borderRight: bW, borderBottom: bS });
+        $tablogoSynheon.css({
+          borderBottom: bW,
+          borderRight: bS,
+        });
+        $tablogoGraumann.css({ borderRight: bS });
+      } else if (_this.hasClass('logo_synheon')) {
+        _this.find('.tablogo').css({ borderRight: bW, borderBottom: bS });
+        $tablogoZoon.css({
+          borderRight: bS,
+          borderBottom: bS,
+        });
+        $tablogoGraumann.css({ borderRight: bS });
+      } else if (_this.hasClass('logo_graumann')) {
+        _this.find('.tablogo').css({ borderRight: bW });
+        $tablogoZoon.css({ borderBottom: bW });
+        $tablogoSynheon.css({ borderRight: bS });
+      }
+    }
   });
 
   // Sliders
   $('.catalog-slider').owlCarousel({
     loop: true,
     nav: true,
-    navText: ['<span class="icon-slider-arrow-left"></span>', '<span class="icon-slider-arrow-right"></span>'],
+    navText: [
+      '<span class="icon-slider-arrow-left"></span>',
+      '<span class="icon-slider-arrow-right"></span>',
+    ],
   });
 
   $('.mfiles-slider').owlCarousel({
     loop: true,
     nav: true,
-    navText: ['<span class="icon-slider-arrow-left"></span>', '<span class="icon-slider-arrow-right"></span>'],
-  });
-
-  // CAT LINKS
-  const $catBtn = $('.category__btn');
-
-  $catBtn.on('change', function () {
-    let $dataCat = $(this).attr('data-cat');
-    $('.select_cat option[value="' + $dataCat + '"]')
-      .prop('selected', true)
-      .trigger('change');
+    navText: [
+      '<span class="icon-slider-arrow-left"></span>',
+      '<span class="icon-slider-arrow-right"></span>',
+    ],
   });
 
   // SELECTS
